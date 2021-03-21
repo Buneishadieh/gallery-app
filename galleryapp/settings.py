4cb691a3d331c661586618e8ec238d48a1d102c6
Bunei shadieh <buneishadrack5@gmail.com>
2021-03-21 23:59:19 +0300

    added migrations

--- galleryapp/settings.py
+++ galleryapp/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'galleryapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'gallery',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
     }
 }
 
