e872ca7a9af25670e1c20ff397c968841e7893b7
Bunei shadieh <buneishadrack5@gmail.com>
2021-04-14 18:14:30 +0300

    initial commit

--- gallery/settings.py
+++ gallery/settings.py
@@ -146,3 +146,6 @@ cloudinary.config(
   api_secret = "4dkAVOK4Wtm51301rMBAxuL0vXI" 
 )
 # database configurations
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
